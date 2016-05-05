af56679405aed66fedc72fa8feb14857465ddd31
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-05 19:42:11 +0200

    is_dialog_present.sh can handle any number of window counts

--- scripts/is_dialog_present.sh
+++ scripts/is_dialog_present.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Checks if there is at least one dialog with the arguments of the function
+# as its window title
+
 function is_dialog_present
 {
-  id=`wmctrl -l | egrep "$@" | cut -f 1 -d ' '`
+  id=`wmctrl -l | egrep "$@" | head -n 1 | cut -f 1 -d ' '`
   if [ -z $id ] 
   then
     echo 0
