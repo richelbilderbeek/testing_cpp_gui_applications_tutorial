a95939e6d7f0b75ecf856aeee45f01fff023aa46
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-06 10:44:33 +0200

    Get y of first dialog with name

---
+++ scripts/get_y_of_first_dialog_with_name_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. close_first_dialog_with_name.sh
+. get_y_of_first_dialog_with_name.sh
+
+window_name="(Untitled)"
+
+leafpad &
+sleep 1
+dialog_y=`get_y_of_first_dialog_with_name $window_name`
+
+if [ -z $dialog_y  ] 
+then 
+  echo "Error: could not find window with name "$window_name
+  exit 1
+else
+  echo "OK: found a y of "$dialog_y" of dialog with name '"$window_name"'"
+  close_first_dialog_with_name $window_name > /dev/null
+fi
