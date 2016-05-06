f3dfad70b8d830e621d49d2959c49395f88ea8de
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-06 11:30:52 +0200

    Minimal project script is nice

--- minimal_project/minimal_project.sh
+++ minimal_project/minimal_project.sh
@@ -2,7 +2,8 @@
 myexe="minimal_project"
 dialog_name="Dialog"
 
-# Check if xdotool is installed
+
+echo "Check if xdotool is installed"
 . ../scripts/is_xdotool_present.sh
 if [ $(is_xdotool_present) -eq 0 ]
 then
@@ -11,7 +12,7 @@ then
 fi
 
 
-# Check if wmctrl is installed
+echo "Check if wmctrl is installed"
 . ../scripts/is_wmctrl_present.sh
 if [ $(is_wmctrl_present) -eq 0 ]
 then
@@ -19,30 +20,31 @@ then
   exit 1
 fi
 
-# Check if executable is present
+
+echo "Check if executable is present"
 if [ ! -e $myexe ]
 then
-  echo "Executable '"$myexe"' not found"
+  echo "Executable '"$myexe"' not found, run 'qmake' and 'make' in this folder to create it"
   exit 1
 fi
 
-# Start the testing
+
 echo "Starting the application"
-$myexe &
+./$myexe &
 sleep 1
 
-# Test if the dialog is found
+
+echo "Check if the dialog can be found"
 . ../scripts/get_dialog_id.sh
 id=`get_dialog_id $dialog_name`
-if [ $id ] 
+if [ ! $id ] 
 then 
-  echo "OK: detected window with id "$id
-else
   echo "Error: should detect window with name"$dialog_name
   exit 1
 fi
 
-# Close the dialog using ALT-F4
+
+echo "Close the dialog using ALT-F4"
 . ../scripts/close_first_dialog_with_name.sh
 error=`close_first_dialog_with_name $dialog_name`
 if [ ! $error -eq 0 ]
@@ -51,37 +53,17 @@ then
   exit 1
 fi
 
+
+
 echo "Starting the application again"
-$myexe &
+./$myexe &
 sleep 1
 
-# Obtaining the window geometry
-echo `xdotool getwindowgeometry $id`
 
-
-exit
-
-# Extracting the dimensions
-. ../scripts/get_x_of_first_dialog_with_name.sh
-. ../scripts/get_y_of_first_dialog_with_name.sh
-. ../scripts/get_width_of_first_dialog_with_name.sh
-. ../scripts/get_height_of_first_dialog_with_name.sh
-x=`get_x_of_first_dialog_with_name $dialog_name`
-y=`get_y_of_first_dialog_with_name $dialog_name`
-w=`get_width_of_first_dialog_with_name $dialog_name`
-h=`get_height_of_first_dialog_with_name $dialog_name`
-echo "x: "$x
-echo "y: "$y
-echo "width:"$w
-echo "height: "$h
-
-# Calculating the position of where to put the mouse, to be able to click on the cross
-mouse_x=$(($x+$w-16))
-mouse_y=$(($y-32))
-echo "where to click, x coordinat: "$mouse_x
-echo "where to click, y coordinat: "$mouse_y
-
-echo "Closing the dialog using a mouse click"
-xdotool windowactivate $id sleep 0.1 mousemove $mouse_x $mouse_y click 1
-
-exit 0
+echo "Close the dialog by setting the mouse at the closing glyph and clicking"
+. ../scripts/set_mouse_at_close_glyph_of_first_dialog_with_name.sh
+set_mouse_at_close_glyph_of_first_dialog_with_name $dialog_name
+sleep 1 # For suspense
+# Set the mouse at the closing glyph again (in case the user moved the mouse)
+set_mouse_at_close_glyph_of_first_dialog_with_name $dialog_name
+xdotool click 1
